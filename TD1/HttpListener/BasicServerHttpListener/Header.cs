816098f9ec3607e8fbb796c2ba838a8f880cc3af
YannMouchon <yannmouchon@gmail.com>
2021-02-17 13:53:57 +0100

    Better header class

--- TD1/HttpListener/BasicServerHttpListener/Header.cs
+++ TD1/HttpListener/BasicServerHttpListener/Header.cs
@@ -12,7 +12,23 @@ namespace BasicServerHTTPlistener
     {
         public void GetHeader(HttpListenerRequest request)
         {
-            Console.WriteLine("Header Accept: " + request.Headers[HttpRequestHeader.Accept.ToString()]);
+            // !!!!!!!!!!!!!!!!!!!!!!!!
+            // example found on https://docs.microsoft.com/en-us/dotnet/api/system.net.httplistenerrequest.headers?view=net-5.0
+
+            System.Collections.Specialized.NameValueCollection headers = request.Headers;
+            // Get each header and display each value.
+            foreach (string key in headers.AllKeys)
+            {
+                string[] values = headers.GetValues(key);
+                if (values.Length > 0)
+                {
+                    Console.WriteLine("The values of the {0} header are: ", key);
+                    foreach (string value in values)
+                    {
+                        Console.WriteLine("   {0}", value);
+                    }
+                }
+            }
         }
     }
 }
