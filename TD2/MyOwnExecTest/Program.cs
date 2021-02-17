52af60ef26b79fbb47039572e81b64552fb59ba8
YannMouchon <yannmouchon@gmail.com>
2021-02-17 17:08:07 +0100

    Add own folder

---
+++ TD2/MyOwnExecTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyOwnExecTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+                Console.WriteLine("<HTML><BODY> " + args[0] + " et " + args[1] + " </BODY></HTML>");
+            else
+                Console.WriteLine("ExeTest <string parameter1> <string parameter2>");
+            Console.ReadLine();
+        }
+    }
+}
