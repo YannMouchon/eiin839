5e2220e06c62edec369d18e995efff07ffe1d22f
YannMouchon <54979073+YannMouchon@users.noreply.github.com>
2021-02-20 19:20:37 +0100

    Update Program.cs

--- TD2/CallPythonScript/Program.cs
+++ TD2/CallPythonScript/Program.cs
@@ -8,9 +8,12 @@ namespace CallPythonScript
         static void Main(string[] args)
         {
             string fileName = @"C:\Users\yannm\Documents\SI4\SOC\eiin839\TD2\CallPythonScript\externalScript.py";
+            // Ici je donne le path de mon script Python
 
             Process p = new Process();
             p.StartInfo = new ProcessStartInfo(@"C:\\Users\\yannm\\AppData\\Local\\Programs\\Python\\Python37\\python.exe", fileName)
+            // Ici je donne le path de mon interpreteur Python. Pour le trouver, j'ai executer la commande "os.path.dirname(sys.executable)" dans mon interpreteur Python 
+            // C'est une étape supplémentaire lorsqu'on pas d'executable.
             {
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
@@ -18,7 +21,7 @@ namespace CallPythonScript
             };
             p.Start();
 
-            string output = p.StandardOutput.ReadToEnd();
+            string output = p.StandardOutput.ReadToEnd(); // je viens récupérer la sortie de mon script pour le rediriger sur la sortie standard du programme en C#
             p.WaitForExit();
 
             Console.WriteLine(output);
