0074df8f35777f857d6f2f131f44ccdaaaf4ed2d
YannMouchon <54979073+YannMouchon@users.noreply.github.com>
2021-02-21 20:37:21 +0100

    Update HeadersReader.cs

--- TD2/WebDynamic/MyOwnHttpListener/HeadersReader.cs
+++ TD2/WebDynamic/MyOwnHttpListener/HeadersReader.cs
@@ -9,13 +9,13 @@ namespace MyOwnHttpListener
         public void GetHeader(HttpListenerRequest request)
         {
             //!!!!!!!!!!!!!!!!!
-            // example found on https://docs.microsoft.com/en-us/dotnet/api/system.net.httplistenerrequest.headers?view=net-5.0
+            // Exemple trouvé sur https://docs.microsoft.com/en-us/dotnet/api/system.net.httplistenerrequest.headers?view=net-5.0
             System.Collections.Specialized.NameValueCollection headers = request.Headers;
-            // Get each header and display each value.
+            // On affiche tous les headers qui existe pour une requete HTTP faite sur notre localhost:8080 ou localhost:8081
             foreach (string key in headers.AllKeys)
             {
                 string[] values = headers.GetValues(key);
-                if (values.Length > 0)
+                if (values.Length > 0) // Vérifie que le header a bien une valeur pour afficher sa valeur. Sinon n'affiche pas le nom du header en question
                 {
                     Console.WriteLine("The values of the {0} header are: ", key);
                     foreach (string value in values)
