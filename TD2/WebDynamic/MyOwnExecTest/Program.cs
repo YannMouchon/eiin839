2303921fe9be5c2091c26eaa84192eb5cab49152
YannMouchon <54979073+YannMouchon@users.noreply.github.com>
2021-02-21 20:31:02 +0100

    Update Program.cs

--- TD2/WebDynamic/MyOwnExecTest/Program.cs
+++ TD2/WebDynamic/MyOwnExecTest/Program.cs
@@ -2,15 +2,15 @@
 
 namespace MyOwnExecTest
 {
-    class Program
+    class Program // Il s'agit juste de générer un executable qui sera ensuite appelé depuis un autre programme
     {
         static void Main(string[] args)
         {
             if (args.Length == 2)
-                Console.WriteLine("<HTML><BODY> " + args[0] + " et " + args[1] + " </BODY></HTML>");
+                Console.WriteLine("<HTML><BODY> " + args[0] + " et " + args[1] + " </BODY></HTML>"); 
             else
-                Console.WriteLine("ExeTest <string parameter1> <string parameter2>");
-            Console.ReadLine();
+                Console.WriteLine("ExeTest <string parameter1> <string parameter2>"); // Si l'executable n'a pas exactement 2 arguments passés en paramètre, alors on montre l'usage de l'executable
+            Console.ReadLine(); // fait en sorte que la console ne se referme pas immédiatement
         }
     }
 }
