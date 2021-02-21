15a621719b6830aa3c62a637f85b2636adcf5779
YannMouchon <54979073+YannMouchon@users.noreply.github.com>
2021-02-21 20:34:35 +0100

    Update Program.cs

--- TD2/WebDynamic/MyOwnExternalExeCall/Program.cs
+++ TD2/WebDynamic/MyOwnExternalExeCall/Program.cs
@@ -4,26 +4,23 @@ using System.IO;
 
 namespace MyOwnExternalExeCall
 {
-    class Program
+    class Program // Le but de ce programme est d'utiliser un executable externe et récupérer ce qui a été écrit sur la sortie standard de l'executable pour la rediriger sur la sortie standard de notre programme
     {
         static void Main(string[] args)
         {
-            //
-            // Set up the process with the ProcessStartInfo class.
-            // https://www.dotnetperls.com/process
-            //
+   
             ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = @"C:\Users\yannm\Documents\SI4\SOC\eiin839\TD2\WebDynamic\MyOwnExecTest\bin\Debug\netcoreapp3.1\MyOwnExecTest.exe"; // Specify exe name.
-            start.Arguments = "géantVert Johhhhhhhny"; // Specify arguments.
+            start.FileName = @"C:\Users\yannm\Documents\SI4\SOC\eiin839\TD2\WebDynamic\MyOwnExecTest\bin\Debug\netcoreapp3.1\MyOwnExecTest.exe"; // Le chemin de l'executable que l'on souhaite appeler.
+            start.Arguments = "géantVert Johhhhhhhny"; // Les arguments que l'on passe à l'executable
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
             //
-            // Start the process.
+            // On démarre le process qui appelle l'executable
             //
             using (Process process = Process.Start(start))
             {
                 //
-                // Read in all the text from the process with the StreamReader.
+                // On récupère la sortie standard de l'executable pour la rediriger sur la sortie de notre programme
                 //
                 using (StreamReader reader = process.StandardOutput)
                 {
