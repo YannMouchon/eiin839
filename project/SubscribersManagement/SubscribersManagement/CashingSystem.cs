e23800d0d874704b5cd9853375552552f6a467ec
YannMouchon <yannmouchon@gmail.com>
2021-03-31 16:28:26 +0200

    Add simple cache

---
+++ project/SubscribersManagement/SubscribersManagement/CashingSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Caching;
+
+namespace SubscribersManagement
+{
+    class CashingSystem
+    {
+        ObjectCache cache = MemoryCache.Default;
+
+        public void AddToCache(string name, string duration)
+        {
+            var cacheItemPolicy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(Double.Parse(duration))
+            };
+            cache.Add(name, name, cacheItemPolicy);
+        }
+
+        public void PrintCache()
+        {
+            DateTime dt = DateTime.Now;
+
+            Console.WriteLine("All key-values at " + dt.Second);
+            //loop through all key-value pairs and print them
+            foreach (var item in cache)
+            {
+                Console.WriteLine("cache object key-value: " + item.Key + "-" + item.Value);
+            }
+        }
+    }
+}
---
+++ project/SubscribersManagement/SubscribersManagement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Caching;
+
+namespace SubscribersManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashingSystem cache = new CashingSystem();
+
+            while(true)
+            {
+                Console.WriteLine("Enter username and duration (Type q to quit): ");
+                string input = Console.ReadLine();
+
+                if (input.Equals("q")) break;
+
+                string[] usersInfo = input.Split();
+                cache.AddToCache(usersInfo[0], usersInfo[1]);
+                cache.PrintCache();
+            }
+        }
+    }
+}
